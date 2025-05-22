b636019188d7a40e44b5a970237fbe5ff2e6794b
su <lk19931990@163.com>
2025-05-22 20:05:06 +0800

    init

---
+++ src/main/kotlin/com/qing/hiKotlin/HiKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.qing.hiKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HiKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<HiKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/qing/hiKotlin/HiKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.qing.hiKotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HiKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
